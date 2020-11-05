224a5c237d5f9b796d1259757116fc410a145737
Arturas Lebedevas <monoidcat@gmail.com>
2020-11-05 11:01:02 +0100

    minor syntax refactor

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 [![Hackage](https://img.shields.io/hackage/v/maml.svg?logo=haskell)](https://hackage.haskell.org/package/maml)
 [![MIT license](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
 
-See README for more info
+## Key features
+
+- Refinement types
+- ...
--- src/Maml/Parser.hs
+++ src/Maml/Parser.hs
@@ -49,7 +49,7 @@ pTypeExpr = makeExprParser term ops
     ops = [[binary "*" Prod], [binary "+" Sum], [binary "->" Arrow]]
 
 pBind :: Parser TExpr
-pBind = curly $ Bind <$> pVarId <* symbol ":" <*> pTypeExpr
+pBind = parens $ Bind <$> pVarId <* symbol ":" <*> pTypeExpr
 
 cons :: Name -> (CExpr -> TCons) -> Parser TCons
 cons name f = f <$> (symbol name *> pExpr)
